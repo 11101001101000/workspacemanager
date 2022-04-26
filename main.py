a841e82160c61697b030bf7046ad855d556b49c8
11101001101000 <t-h-0928@naver.com>
2022-04-26 13:39:58 +0900

    update

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 end = 0
+os=''
 while end==0:
     print('''Welcome to WorkspaceManager.
 1) view workspace structure
 2) create new workspace
 3) delete existing workspace''')
-    a = input()
+    a = str(input())
+    if a=='1':
+        
+    elif a=='2':
 
+    elif a == '3':
+        
