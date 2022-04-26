d4a8e7c649620bf10aaad6a6623c54c6ff0380f0
11101001101000 <t-h-0928@naver.com>
2022-04-26 16:31:58 +0900

    update

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ while end==0:
 3) delete existing workspace''')
     a = str(input())
     if a=='1':
-        
+        print ('''windows or unix-like:
+w) wibdows
+u) unix-like (linux, bsd, etc.) ('ls')
     elif a=='2':
 
     elif a == '3':
