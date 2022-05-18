0bc963e7b959b4ea25281197354cf0f487108b8f
11101001101000 <t-h-0928@naver.com>
2022-05-18 22:19:05 +0900

    added option 4 and 5

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+import os as exec
 end = 0
-while end==0:
-    print('''Welcome to WorkspaceManager.
-1) view workspace structure
+os = 0
+hd = 0
+a = 0
+while end == 0:
+    if os == 0 or hd == 0:
+        os = input ('which os are you on, win or linux?  ')
+        hd = input ('workspace dir path:  ')
+        print ('Welcome to WorkspaceManager.')
+    print('''1) view workspace structure
 2) create new workspace
-3) delete existing workspace''')
+3) delete existing workspace
+4) change os
+5) change workspace path''')
     a = input()
-
+    if a == '1':
+        if os == 'win':
+            exec.system ('dir ' + hd)
+    if a == '4':
+        os = 0
+    if a == '5':
+        hd = 0
